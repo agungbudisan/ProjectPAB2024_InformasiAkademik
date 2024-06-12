9625105f96879c758e27f7b5bf0d4c875ea4eb6b
agungbudisan <agungbudisantoso@student.uns.ac.id>
2024-06-12 15:43:14 +0700

    Commit pertama

---
+++ app/src/main/java/com/pab2024klsa/informasiakademik/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.pab2024klsa.informasiakademik.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/pab2024klsa/informasiakademik/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.pab2024klsa.informasiakademik.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
