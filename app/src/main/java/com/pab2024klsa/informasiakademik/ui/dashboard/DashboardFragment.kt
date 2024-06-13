ed4187fb51b8cef605c69156c38058a5d64ee1b6
CukupBani <paus.biru015@gmail.com>
2024-06-13 11:17:12 +0700

    Add Dashboard Page

--- app/src/main/java/com/pab2024klsa/informasiakademik/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/pab2024klsa/informasiakademik/ui/dashboard/DashboardFragment.kt
@@ -4,39 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.pab2024klsa.informasiakademik.databinding.FragmentDashboardBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pab2024klsa.informasiakademik.R
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var rvDashboard: RecyclerView
+    private val list = ArrayList<DashboardButton>()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-        return root
+
+        rvDashboard = view.findViewById(R.id.rv_dashboard)
+        rvDashboard.setHasFixedSize(true)
+
+        list.addAll(getListCars())
+        showRecyclerList()
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun getListCars(): ArrayList<DashboardButton> {
+        val dataBtnTitle = resources.getStringArray(R.array.data_btn_title)
+        val listDashboard = ArrayList<DashboardButton>()
+        for (i in dataBtnTitle.indices) {
+            val dashboard = DashboardButton(dataBtnTitle[i])
+            listDashboard.add(dashboard)
+        }
+        return listDashboard
+    }
+
+    private fun showRecyclerList() {
+        rvDashboard.layoutManager = LinearLayoutManager(requireContext())
+        val listDashboardAdapter = ListDashboardAdapter(list)
+        rvDashboard.adapter = listDashboardAdapter
+
+        listDashboardAdapter.setOnItemClickCallback(object : ListDashboardAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: DashboardButton) {
+                showSelectedDashboardButton(data)
+            }
+        })
+    }
+
+    private fun showSelectedDashboardButton(dashboardButton: DashboardButton) {
+        Toast.makeText(requireContext(), dashboardButton.btnTitle + " is selected", Toast.LENGTH_SHORT).show()
     }
 }
